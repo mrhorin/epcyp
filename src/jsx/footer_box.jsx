ab248d48c636ddda52bcb2ba5ace646af6e9c794
mrhorin <u0813mb@gmail.com>
2016-12-30 01:59:52 +0900

    footerのカウンターをコンポーネント化

--- src/jsx/footer_box.jsx
+++ src/jsx/footer_box.jsx
@@ -1,52 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import FooterCount from "jsx/footer_count"
 
 module.exports = class FooterBox extends React.Component {
-
-  constructor(props){
-    super(props)
-    this.startUpdateTimer = this.startUpdateTimer.bind(this)
-    this.state = {
-      autoUpdateCount: this.props.autoUpdateCount
-    }
-    this.startUpdateTimer()
-  }
-
-  // 自動更新タイマーの開始
-  startUpdateTimer(){
-    this.countTimer = setInterval(()=>{
-      if(this.props.autoUpdate){
-        // 自動更新ON時の処理
-        if(this.state.autoUpdateCount < 1){
-          this.props.onUpdateHandler()
-          this.setState({ autoUpdateCount: this.props.autoUpdateCount })
-        }else{
-          this.setState({autoUpdateCount: this.state.autoUpdateCount -1})
-        }
-      }else{
-        // 自動更新OFF時の処理
-        if(this.state.autoUpdateCount != this.props.autoUpdateCount){
-          this.setState({ autoUpdateCount: this.props.autoUpdateCount })
-        }
-      }
-    }, 1000)
-  }
-
-  // mm:ssに変換
-  toTimeFormat(secounds){
-    let min = Math.floor((secounds / 60) % 60)
-    if(min<10) min = `0${min}`
-    let sec = Math.floor(secounds % 60)
-    if(sec<10) sec = `0${sec}`
-    return `${min}:${sec}`
-  }
 
   render(){
     return(
       <footer className="toolbar toolbar-footer">
-        <div className="footer-right">
-          {this.toTimeFormat(this.state.autoUpdateCount)}
-        </div>
+        <FooterCount autoUpdate={this.props.autoUpdate} autoUpdateCount={this.props.autoUpdateCount} onUpdateHandler={this.props.onUpdateHandler}/>
       </footer>
     )
   }
---
+++ src/jsx/footer_count.jsx
@@ -0,0 +1,51 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+module.exports = class FooterCount extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.startUpdateTimer = this.startUpdateTimer.bind(this)
+    this.state = {
+      autoUpdateCount: this.props.autoUpdateCount
+    }
+    this.startUpdateTimer()
+  }
+
+  // 自動更新タイマーの開始
+  startUpdateTimer(){
+    this.countTimer = setInterval(()=>{
+      if(this.props.autoUpdate){
+        // 自動更新ON時の処理
+        if(this.state.autoUpdateCount < 1){
+          this.props.onUpdateHandler()
+          this.setState({ autoUpdateCount: this.props.autoUpdateCount })
+        }else{
+          this.setState({autoUpdateCount: this.state.autoUpdateCount -1})
+        }
+      }else{
+        // 自動更新OFF時の処理
+        if(this.state.autoUpdateCount != this.props.autoUpdateCount){
+          this.setState({ autoUpdateCount: this.props.autoUpdateCount })
+        }
+      }
+    }, 1000)
+  }
+
+  // mm:ssに変換
+  toTimeFormat(secounds){
+    let min = Math.floor((secounds / 60) % 60)
+    if(min<10) min = `0${min}`
+    let sec = Math.floor(secounds % 60)
+    if(sec<10) sec = `0${sec}`
+    return `${min}:${sec}`
+  }
+
+  render(){
+    return(
+      <div className="footer-right">
+        {this.toTimeFormat(this.state.autoUpdateCount)}
+      </div>
+    )
+  }
+}
