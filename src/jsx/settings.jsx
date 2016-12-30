5821b62e1da636416c74ec247be642b8df3caf07
mrhorin <u0813mb@gmail.com>
2016-12-31 00:35:36 +0900

    プレイヤーパスをダイアログで指定できるように

--- src/jsx/settings.jsx
+++ src/jsx/settings.jsx
@@ -1,8 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import {ipcRenderer} from "electron"
+import {remote} from 'electron'
 import css from "scss/style"
 import Config from 'electron-config'
+const dialog = remote.dialog
 const config = new Config({
   defaults: { port: 7144, player: "" }
 })
@@ -14,34 +16,36 @@ class Settings extends React.Component {
     this.save = this.save.bind(this)
     this.onChangePort = this.onChangePort.bind(this)
     this.onChangePlayer = this.onChangePlayer.bind(this)
+    this.onClickPlayerDialog = this.onClickPlayerDialog.bind(this)
     this.state = {
       port: config.get('port'),
       player: config.get('player')
     }
-    console.log(this.state)
   }
 
   // 設定保存
   save(){
     config.set('port', this.state.port)
     config.set('player', this.state.player)
-    ipcRenderer.send('asyn-config-window-close')
+    this.close()
   }
 
+  // 設定ウィンドウを閉じる
   close(){
     ipcRenderer.send('asyn-config-window-close')
   }
 
   onChangePort(event){
-    this.setState({
-      port: event.target.value
-    })
+    this.setState({ port: event.target.value })
   }
 
   onChangePlayer(event){
-    this.setState({
-      player: event.target.value
-    })
+    this.setState({ player: event.target.value })
+  }
+
+  onClickPlayerDialog(){
+    let path = dialog.showOpenDialog()
+    this.setState({ player: path })
   }
 
   render(){
@@ -54,16 +58,15 @@ class Settings extends React.Component {
           </h1>
         </header>
         <div id="settings-main">
-          <form className="configForm">
-            <div className="form-group">
-              <label>ポート番号</label>
-              <input type="text" className="form-control" ref="port" value={this.state.port} onChange={this.onChangePort} />
-            </div>
-            <div className="form-group">
-              <label>プレイヤーパス</label>
-              <input type="text" className="form-control" ref="player" value={this.state.player} onChange={this.onChangePlayer} />
-            </div>
-          </form>
+          <div className="form-group">
+            <label>ポート番号</label>
+            <input type="text" className="form-control" ref="port" value={this.state.port} onChange={this.onChangePort} />
+          </div>
+          <div className="form-group">
+            <label>再生プレイヤー</label>
+            <input id="settings-player" type="text" className="form-control" ref="player" value={this.state.player} onChange={this.onChangePlayer} />
+            <button id="settings-player-dialog" className="btn btn-default" onClick={this.onClickPlayerDialog}>参照</button>
+          </div>
           <button id="settings-ok" className="btn btn-default" onClick={this.save}>OK</button>
           <button id="settings-cancel" className="btn btn-default" onClick={this.close}>キャンセル</button>
         </div>
