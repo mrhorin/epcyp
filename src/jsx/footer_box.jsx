27118bd7096411158cb022c0b06bd0411f8218dd
mrhorin <u0813mb@gmail.com>
2017-01-07 16:26:08 +0900

    最終更新時のフォーマットをhh:mmにするように

--- src/jsx/footer_box.jsx
+++ src/jsx/footer_box.jsx
@@ -4,11 +4,20 @@ import FooterCount from 'jsx/footer_count'
 
 module.exports = class FooterBox extends React.Component {
 
+  // hh:mmに変換
+  toTimeFormat(date){
+    let hour = date.getHours()
+    if(hour<10) hour = '0'+hour
+    let min = date.getMinutes()
+    if(min<10) min = '0'+min
+    return `${hour}:${min}`
+  }
+
   render(){
     return(
       <footer className="toolbar toolbar-footer">
         <div className="footer-left">
-          {`${this.props.lastUpdateTime.getHours()}:${this.props.lastUpdateTime.getMinutes()}`}
+          {this.toTimeFormat(this.props.lastUpdateTime)}
         </div>
         <FooterCount autoUpdate={this.props.autoUpdate} autoUpdateCount={this.props.autoUpdateCount} lastUpdateTime={this.props.lastUpdateTime}
           onUpdate={this.props.onUpdate}/>
