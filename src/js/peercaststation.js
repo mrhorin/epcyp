8e48261ce37cb5c2ce0868b9bf8935708e22c48f
mrhorin <u0813mb@gmail.com>
2017-01-19 10:09:07 +0900

    PeerCastStationを操作するクラスを実装

---
+++ src/js/peercaststation.js
@@ -0,0 +1,47 @@
+import request from 'superagent'
+import Config from 'electron-config'
+
+const config = new Config({ defaults: { port: 7144 } })
+
+/*-----------------------------------------------------------------
+  PeerCastStionを操作するクラス
+  参考: PeerCastStation.UI.HTTP/html/js/peercaststation.1.0.0.js
+------------------------------------------------------------------*/
+module.exports = class Peercaststion{
+
+  constructor(){
+  }
+
+  // チャンネル情報一覧を取得
+  static getChannels(call=()=>{}){
+    this.postRequest('getChannels', null, (err, res)=>{
+      call(err, res)
+    })
+  }
+
+  static get API_URL(){
+    return 'http://127.0.0.1:'+config.get('port')+'/api/1'
+  }
+
+  // 4桁のランダムな数値IDを取得
+  static get getRandomId(){
+    return Math.floor(Math.random()*10000)
+  }
+
+  // APIへリクエスト
+  static postRequest(method, params=null, call=()=>{}){
+    let json = {
+      jsonrpc: '2.0',
+      id: this.getRandomId,
+      method: method
+    }
+    if(params) json.params = params
+    request.post(this.API_URL).send(JSON.stringify(json))
+      .set('X-Requested-With', 'X-Requested-With')
+      .set('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+      .end((err, res)=>{
+        call(err, res)
+      })
+  }
+
+}
