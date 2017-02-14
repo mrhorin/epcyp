b5be6835c882e75f78f71a4697f954517a718112
mrhorin <u0813mb@gmail.com>
2017-02-14 16:12:19 +0900

    検索フォームにキャンセルボタンを追加

--- src/jsx/index/header_search.jsx
+++ src/jsx/index/header_search.jsx
@@ -6,6 +6,7 @@ module.exports = class HeaderSearch extends React.Component {
   constructor(props){
     super(props)
     this.search = this.search.bind(this)
+    this.cancel = this.cancel.bind(this)
   }
 
   search(event){
@@ -14,11 +15,18 @@ module.exports = class HeaderSearch extends React.Component {
     }
   }
 
+  cancel(){
+    this.refs.searchWord.value = ""
+  }
+
   render(){
     return(
       <div className="search-group">
-        <input ref="searchWord" type="text" onKeyPress={this.search} />
-        <button className="btn btn-default" onKeyDown={this.search} onClick={this.search}>
+        <input id="search-word" ref="searchWord" type="text" onKeyPress={this.search} />
+        <button id="search-cancel" className="btn btn-default" onKeyDown={this.search} onClick={this.cancel}>
+          <span className="icon icon-cancel" />
+        </button>
+        <button id="search-submit" className="btn btn-default" onKeyDown={this.search} onClick={this.search}>
           <span className="icon icon-search" />
         </button>
       </div>
