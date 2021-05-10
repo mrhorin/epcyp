5d2ed69b808c24a7a5f4551192734209bb2285d7
mrhorin <u0813mb@gmail.com>
2021-05-10 19:54:34 +0900

    ポート開放状態をアイコンで表示するようにした

--- src/jsx/index/footer_box.jsx
+++ src/jsx/index/footer_box.jsx
@@ -8,18 +8,15 @@ export default class FooterBox extends React.Component {
   render() {
     let portStatus
     if (this.props.isFirewalled === null) {
-      portStatus = "?"
+      portStatus = <span className="icon icon-help"></span>
     } else if (this.props.isFirewalled === true) {
-      portStatus = "NG"
+      portStatus = <span className="icon icon-cancel"></span>
     }else if (this.props.isFirewalled === false) {
-      portStatus = "OK"
+      portStatus = <span className="icon icon-check"></span>
     }
     return(
       <footer className="toolbar toolbar-footer">
-        <div className="footer-icons">
-          Port: {portStatus}
-          {/* <span className="icon icon-megaphone"></span> */}
-        </div>
+        <div className="footer-icons">{portStatus}</div>
         <FooterCount
           isAutoUpdate={this.props.isAutoUpdate}
           autoUpdateCount={this.props.autoUpdateCount}
